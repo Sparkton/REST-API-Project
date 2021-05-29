a1c9527cc21c94c6fe3d634d244185cde26afa2c
Sparkton <akash1999dubey@gmail.com>
2021-05-29 21:15:29 +0530

    Backend, Server Complete

--- app.js
+++ app.js
@@ -1,20 +1,68 @@
 //const bodyParser = require('body-parser');
+const path = require('path');
+
 const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
 app.use(express.json());
 //app.use(express.urlencoded({extended: true}));
+app.use(
+	multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+  );
+  app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use ((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUSH, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
-    next();
+const fileStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+	  cb(null, 'images');
+	},
+	filename: (req, file, cb) => {
+	  cb(null, new Date().toISOString() + '-' + file.originalname);
+	}
+  });
+
+  const fileFilter = (req, file, cb) => {
+	if (
+	  file.mimetype === 'image/png' ||
+	  file.mimetype === 'image/jpg' ||
+	  file.mimetype === 'image/jpeg'
+	) {
+	  cb(null, true);
+	} else {
+	  cb(null, false);
+	}
+  };
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUSH, PATCH, DELETE"
+	);
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	next();
 });
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
+app.use('/auth', authRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+	console.log(error);
+	const status = error.statusCode || 500;
+	const message = error.message;
+	const data = error.data;
+	res.status(status).json({message: message, data: data});
+});
+
+mongoose
+	.connect("mongodb+srv://akash:akash@cluster0.l7zs9.mongodb.net/Project0")
+	.then(result => {
+        app.listen(8080);
+    })
+	.catch(err => console.log(err));
