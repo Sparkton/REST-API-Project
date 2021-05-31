a0bc8b9f9fbb557c4ce74377a50ba77fa8956a91
Sparkton <akash1999dubey@gmail.com>
2021-05-31 15:08:51 +0530

    Impl web sockets, async-await, REST finished

--- app.js
+++ app.js
@@ -1,43 +1,44 @@
 //const bodyParser = require('body-parser');
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const mongoose = require('mongoose');
-const multer = require('multer');
+const express = require("express");
+const mongoose = require("mongoose");
+const multer = require("multer");
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
+const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
+
+const fileStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "images");
+	},
+	filename: (req, file, cb) => {
+		cb(null, new Date().toISOString() + "-" + file.originalname);
+	},
+});
+
+
+const fileFilter = (req, file, cb) => {
+	if (
+		file.mimetype === "image/png" ||
+		file.mimetype === "image/jpg" ||
+		file.mimetype === "image/jpeg"
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
 
 app.use(express.json());
 //app.use(express.urlencoded({extended: true}));
 app.use(
-	multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
-  );
-  app.use('/images', express.static(path.join(__dirname, 'images')));
+	multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 
-const fileStorage = multer.diskStorage({
-	destination: (req, file, cb) => {
-	  cb(null, 'images');
-	},
-	filename: (req, file, cb) => {
-	  cb(null, new Date().toISOString() + '-' + file.originalname);
-	}
-  });
-
-  const fileFilter = (req, file, cb) => {
-	if (
-	  file.mimetype === 'image/png' ||
-	  file.mimetype === 'image/jpg' ||
-	  file.mimetype === 'image/jpeg'
-	) {
-	  cb(null, true);
-	} else {
-	  cb(null, false);
-	}
-  };
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
@@ -50,19 +51,23 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
 	console.log(error);
 	const status = error.statusCode || 500;
 	const message = error.message;
 	const data = error.data;
-	res.status(status).json({message: message, data: data});
+	res.status(status).json({ message: message, data: data });
 });
 
 mongoose
 	.connect("mongodb+srv://akash:akash@cluster0.l7zs9.mongodb.net/Project0")
-	.then(result => {
-        app.listen(8080);
-    })
-	.catch(err => console.log(err));
+	.then((result) => {
+		const server = app.listen(8080);
+		const io = require("./socket").init(server);
+		io.on("connection", (socker) => {
+			console.log("Client connected");
+		});
+	})
+	.catch((err) => console.log(err));
